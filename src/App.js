530f21d07e5aabb230646c42cd668ec54d88996b
MohitSainiDev <mht4943@gmail.com>
2024-02-13 15:47:38 +0530

    Counter example

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@
 import './App.css';
 import LearningComponent from './components/learning-examples/LearningComponent'
 import LearningJavaScript from './components/learning-examples/LearningJavaScript';
+import Counter from './components/counter/Counter';
 
 function App() {
   return (
     <div className="App">
-      <LearningComponent/>
-      <LearningJavaScript/>
+      <Counter/>
     </div>
   );
 }
